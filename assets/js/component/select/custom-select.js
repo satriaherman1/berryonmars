123383fe41e8e21ab1677e322e1800f2c34c3916
satriaherman <muhammadsatriaherman@gmail.com>
2021-09-11 17:33:12 +0700

    create first page

--- assets/js/component/select/custom-select.js
+++ assets/js/component/select/custom-select.js
@@ -1,9 +1,24 @@
 const customSelectField = document.querySelectorAll('.custom-select-field')
+const allSelect = document.querySelectorAll('.custom-select')
 
 const optionListClick = (opt, index) => {
-    const currentSelect = customSelectField[index].querySelector('select')
-    currentSelect.value = opt.textContent
+    const currentSelect = customSelectField[index].querySelector('select');
+    currentSelect.value = opt.textContent;
+    opt.parentElement.classList.add('d-none')
 }
+
+const selectClick = (select) => {
+    const currentCustomSelectList = select.parentElement.querySelector('.custom-select-list')
+
+    currentCustomSelectList.classList.toggle('d-none')
+}
+
+window.onload = function(){
+    allSelect.forEach((select) => {
+        select.innerHTML += `<div class="custom-select-arrow"></div>`
+    })
+}
+
 customSelectField.forEach((field , index) => {
     const customSelect = field.querySelector('.custom-select')
     const customSelectList = field.querySelector('.custom-select-list')
@@ -14,6 +29,7 @@ customSelectField.forEach((field , index) => {
             customSelectList.innerHTML += `<li class="custom-option-list" onclick="optionListClick(this , ${index})">${el.value}</li>`
         }
     })
+
 
 })
 
